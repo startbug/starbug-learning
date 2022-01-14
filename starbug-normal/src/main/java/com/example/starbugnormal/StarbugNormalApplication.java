b4f52687b1b44a966d6b4bd5ea9ab96de2927096
lianghaohui <lianghaohui@ly-sky.com>
2022-01-14 17:41:01 +0800

    初始化starbug-normal项目 2022-01-14 17:40:56

---
+++ starbug-normal/src/main/java/com/example/starbugnormal/StarbugNormalApplication.java
@@ -0,0 +1,13 @@
+package com.example.starbugnormal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbugNormalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarbugNormalApplication.class, args);
+    }
+
+}
